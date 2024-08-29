4c34a1d976728978b4e1fe5d77be66f2639645c3
igl-myrick <igl.myrick@gmail.com>
2024-08-29 15:46:34 -0700

    add Noun get functionality

--- MadLibs/Models/GameVariables.cs
+++ MadLibs/Models/GameVariables.cs
@@ -7,5 +7,6 @@ namespace MadLibs.Models
     public string Animal { get; set; }
     public string Exclamation { get; set; }
     public string Verb { get; set; }
+    public string Noun { get; }
   }
 }
